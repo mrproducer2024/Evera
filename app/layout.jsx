c69a26d7101eef0be5bfb071e408d236661d123e
Homayoun <homayoun763@gmail.com>
2024-03-20 21:24:13 +0330

    add yektanet script to code

--- app/layout.jsx
+++ app/layout.jsx
@@ -18,17 +18,13 @@ export const metadata = {
 
  const header = (
     <>
- <script
+        <script
           dangerouslySetInnerHTML={{
             __html: `
-              !function(e,t,n){e.yektanetAnalyticsObject=n,e[n]=e[n]||function(){e[n].q.push(arguments)},e[n].q=e[n].q||[];var a=t.getElementsByTagName("head")[0],r=new Date,c="https://cdn.yektanet.com/superscript/1tavFZLi/native-mangnify.vercel.app-36551/yn_pub.js?v="+r.getFullYear().toString()+"0"+r.getMonth()+"0"+r.getDate()+"0"+r.getHours(),s=t.createElement("link");s.rel="preload",s.as="script",s.href=c,a.appendChild(s);var l=t.createElement("script");l.async=!0,l.src=c,a.appendChild(l)}(window,document,"yektanet");
+            !function(e,t,n){e.yektanetAnalyticsObject=n,e[n]=e[n]||function(){e[n].q.push(arguments)},e[n].q=e[n].q||[];var a=t.getElementsByTagName("head")[0],r=new Date,c="https://cdn.yektanet.com/superscript/z5IH2sOW/native-powecluby.vercel.app-36960/yn_pub.js?v="+r.getFullYear().toString()+"0"+r.getMonth()+"0"+r.getDate()+"0"+r.getHours(),s=t.createElement("link");s.rel="preload",s.as="script",s.href=c,a.appendChild(s);var l=t.createElement("script");l.async=!0,l.src=c,a.appendChild(l)}(window,document,"yektanet");
             `,
           }}
         />
-        <script 
-          src="https://s1.mediaad.org/serve/77542/retargeting.js"
-          async />
-
     </>
   );
 
